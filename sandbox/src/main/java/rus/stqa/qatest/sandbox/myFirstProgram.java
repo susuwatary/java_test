3c4a4025556317550a847a2a07ec8c19ddba7d47
susuwatary <reidarein@gmail.com>
2016-08-26 00:14:07 +0300

    Добавлены новый функции

--- sandbox/src/main/java/rus/stqa/qatest/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/rus/stqa/qatest/sandbox/myFirstProgram.java
@@ -3,9 +3,36 @@ package rus.stqa.qatest.sandbox;
 public class myFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!!! DZ");
+    String somebody = "world";
+    System.out.println("Hello, " + somebody + "!");
+    double l = 0.4;
+    double s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+    hello("Alena");
+
+    double len = 5;
+    System.out.println("Площадь квадрата = " + area(len) );
+
+    double a = 4;
+    double b = 5;
+
+    System.out.println("Площадь прямоуголника = " + area(a, b) );
 
 
   }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello," + somebody +"!");
+  }
+
+  public static double area (double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
+
 
 }
