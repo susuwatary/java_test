fadc403758c659bfbf1f0e74f83f6a1510cfa5a4
susuwatary <reidarein@gmail.com>
2016-08-26 02:13:29 +0300

    Алгоритм вычисления длины точек

--- sandbox/src/main/java/rus/stqa/qatest/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/rus/stqa/qatest/sandbox/myFirstProgram.java
@@ -1,5 +1,7 @@
 package rus.stqa.qatest.sandbox;
 
+import java.awt.*;
+
 public class myFirstProgram {
 
   public static void main(String[] args) {
@@ -7,17 +9,29 @@ public class myFirstProgram {
     System.out.println("Hello, " + somebody + "!");
     hello("Alena");
 
-
     Square s = new Square(5);
     System.out.println("Площадь квадрата = " + s.area());
 
-
-
     Rectangle r = new Rectangle(4, 6);
 
-
-
     System.out.println("Площадь прямоуголника = " + r.area() );
+
+    double p1x = 4;
+    double p1y = 4;
+    double p2x = 9;
+    double p2y = 9;
+    System.out.println("Координаты точкек p1(" + p1x + ";" + p1y + ") и p2(" + p2x + ";" + p2y + ")");
+
+    double p1 = p2x - p1x;
+    double s1 = Math.pow(p1, 2);
+    double p2 = p2y - p1y;
+    double s2 = Math.pow(p2, 2);
+    double sum = s1 +s2;
+    double l = Math.sqrt(sum);
+    System.out.println("Длинна точек p1(" + p1x + ";" + p1y + ") и p2(" + p2x + ";" + p2y + ") = " + l);
+
+
+
 
 
   }
@@ -25,6 +39,9 @@ public class myFirstProgram {
   public static void hello(String somebody) {
     System.out.println("Hello," + somebody +"!");
   }
+
+
+
 
 
 
